a6c6d668450155b6509a46b1ace7535d43788e30
Lucas Duarte <lucashdp@outlook.com>
2018-12-02 21:32:36 -0200

    Implements gzip compression

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./app/routes');
 const app = express();
+var compression = require('compression');
 
 const port = process.env.PORT || 8000;
 
+app.use(compression());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/assets/css', express.static('assets/css'));
 app.use('/assets/icons', express.static('assets/icons'));
